bc927bd50ee3b7ce4570555cb396e5d20afc0032
Brad Herring <brad@bherville.com>
2013-08-29 16:57:11 -0400

    Fixed: CSS formatting of the devise forms.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 admin = User.new                :username       => 'admin',
-                                :email          => 'admin@example.com',
                                 :password       => 'secret'
 
 admin.skip_confirmation!
